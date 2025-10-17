b39f640ebe26db48a4d5466aa58a1721337c3710
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-10-17 22:18:05 +0530

    finalizing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template, request, redirect
 from pipline import preprocessing, vectorizer, get_prediction
 
 
@@ -16,10 +16,26 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
-
-
-
     return render_template('index.html', data=data)
+
+@app.route("/", methods = ['POST'])
+def mypost():
+    text = request.form['text']
+    ppt=preprocessing(text)
+    vd=vectorizer(ppt)
+    pred=get_prediction(vd)
+
+    if pred == 'positive':
+        global positive
+        positive += 1
+    elif pred == 'negative':
+        global negative
+        negative += 1
+
+    reviews.insert(0, text)
+    return redirect(request.url)
+
+
 
 
 if __name__ == "__main__":
